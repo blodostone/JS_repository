e680534e755e7f6976dfdd2a7004e82a94c86ec4
bloodstone <maks.khokholov.99@gmail.com>
2020-03-22 20:25:46 +0200

    jkfgf

---
+++ lesson3/task6/index.js
@@ -0,0 +1,27 @@
+/* Этот код менять не нужно */
+const defaultUserName = 'anonymus';
+const userName = 'John';
+const anotherUser = null;
+const isAdmin = false;
+const message = 'text';
+const anotherMessage = 'text as well';
+const isLoggedIn = Boolean(userName);
+
+/* В коде ниже нужно использовать правильные операторы после знака присваивания */
+const activeUser = defaultUserName /**/ ;
+console.log(activeUser);
+
+const hasAccess = isLoggedIn /**/ ;
+console.log(hasAccess);
+
+const isTruthy = isLoggedIn /**/ ;
+console.log(isTruthy);
+
+const isFalsy = isAdmin /**/ ;
+console.log(isFalsy);
+
+const isNotTrue = isAdmin /**/ ;
+console.log(isNotTrue);
+
+const isTrue = isLoggedIn /**/ ;
+console.log(isTrue);
