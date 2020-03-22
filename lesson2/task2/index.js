d43e2c57ab3bd82d4025ebce0cfba611b0798be5
bloodstone <maks.khokholov.99@gmail.com>
2020-03-22 03:35:54 +0200

    let

---
+++ lesson2/task2/index.js
@@ -0,0 +1,40 @@
+const a = 17;
+console.log('a: 17', typeof a);
+
+const b = 17.17;
+console.log('b: 17.17', typeof b);
+
+const c = '17';
+console.log("c: '17'", typeof c);
+
+const d = "17";
+console.log('d: "17"', typeof d);
+
+const e = "I'm a string";
+console.log('e: "Im a string"', typeof e);
+
+const f = false;
+console.log('f: false', typeof f);
+
+const g = true;
+console.log('g: true', typeof g);
+
+const h = null;
+console.log('h: null', typeof h);
+
+const i = undefined;
+console.log('i: undefind', typeof i);
+
+const j = Symbol("I'm a Symbol");
+console.log('j: Symbol("Im a Symbol"', typeof j);
+
+const k = { name: 'John' };
+console.log('k: { name: "John" }', typeof k);
+
+const l = [1, 2, 3, 4];
+console.log('l: [1, 2, 3, 4]', typeof l);
+
+const m = function run() {
+    return 17;
+};
+console.log('m: function run() { return 17 }', typeof m);
