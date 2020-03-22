88341331a8ff1e6dcc99448d265a3a2c8b16be3d
bloodstone <maks.khokholov.99@gmail.com>
2020-03-22 05:49:31 +0200

    tret

--- lesson2/task4/index.js
+++ lesson2/task4/index.js
@@ -3,7 +3,9 @@ let b = 2;
 console.log('a: ', a);
 console.log('b: ', b);
 
-console.log(a - b);
-console.log(a + b);
+let c = a - b;
+console.log('a - b = ', c);
+let d = a + b;
+console.log('a + b = ', d);
 console.log(a / b);
 console.log(a * b);
