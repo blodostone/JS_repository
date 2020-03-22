b47bbe798b71fa4401a080e8b16a3185d9f7d0ba
bloodstone <maks.khokholov.99@gmail.com>
2020-03-22 05:54:08 +0200

    troll

--- lesson2/task4/index.js
+++ lesson2/task4/index.js
@@ -5,7 +5,12 @@ console.log('b: ', b);
 
 let c = a - b;
 console.log('a - b = ', c);
+
 let d = a + b;
 console.log('a + b = ', d);
-console.log(a / b);
-console.log(a * b);
+
+let f = a / b;
+console.log('a / b = ', f);
+
+let z = a * b;
+console.log('a * b = ', z);
