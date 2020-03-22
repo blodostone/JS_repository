d6fe2b63e1d13224dcbdd8943631de7a465ebbbb
bloodstone <maks.khokholov.99@gmail.com>
2020-03-22 19:07:04 +0200

    boom

---
+++ lesson3/task2/index.js
@@ -0,0 +1,30 @@
+//code should be finished ;)
+
+console.log("17 to string is " + String(17));
+console.log("-17.17 to string is " + String(-17.17));
+console.log("false to string is ");
+console.log("null to string is ");
+console.log("undefined to string is ");
+console.log("0 to string is ");
+
+/* === */
+
+console.log("'17' to number is ");
+console.log("true to number is ");
+console.log("false to number is ");
+console.log("null to number is ");
+console.log("undefined to number is ");
+console.log("'   20   ' to number is ");
+console.log("'      ' to number is ");
+console.log("'   30d   ' to number is ");
+
+/* == */
+console.log("null to boolean is ");
+console.log("undefined to boolean is ");
+console.log("0 to boolean is ");
+console.log("-0 to boolean is ");
+console.log("NaN to boolean is ");
+console.log("'' to boolean is ");
+console.log("' ' to boolean is ");
+console.log("17 to boolean is ");
+console.log("'Hello' to boolean is ");
