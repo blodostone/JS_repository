d2b6470ca90d279b22a9a1e92e0d4b54dbbfc017
bloodstone <maks.khokholov.99@gmail.com>
2020-04-14 12:41:00 +0300

    bn

--- lesson4/task2/index.js
+++ lesson4/task2/index.js
@@ -1,7 +1,7 @@
 const m = 30;
 const n = 40;
 let result = 1;
-let g = 1;
+let g = 2;
 
 do {
     if (g % 2 === 1) {
@@ -9,7 +9,7 @@ do {
     }
 
     g++;
-} while (g <= m);
+} while (g <= n);
 
 
 console.log('Result: ' + result);
