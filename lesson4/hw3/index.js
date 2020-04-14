d0d8be3c1f58e85200448999e7b128e9ad3fc559
bloodstone <maks.khokholov.99@gmail.com>
2020-04-14 21:22:32 +0300

    gfb

---
+++ lesson4/hw3/index.js
@@ -0,0 +1,14 @@
+for (let m = 0; m <= 1000; m++) {
+    if (m % 2 != 0) {
+        sum = 0;
+        sum += m;
+        console.log('Found');
+    }
+
+
+    if (result * 5 > 5000) {
+        console.log("Bigger")
+    } else {
+        console.log('Smaller')
+    }
+}
