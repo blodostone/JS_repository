3b101098c603f23be1b2cfc97286b7611024c819
bloodstone <maks.khokholov.99@gmail.com>
2020-04-14 15:42:25 +0300

    gf

--- lesson4/hw1/index.js
+++ lesson4/hw1/index.js
@@ -1,9 +1,8 @@
 const n = 15;
-let g = 1;
-let sum = 1;
+let sum = "";
 
-while (g <= n) {
-    sum = g;
-    console.log('Result: ' + sum);
-    g++;
+for (let i = 1; i <= n; i++) {
+    sum = sum + i;
 }
+
+console.log(sum);
