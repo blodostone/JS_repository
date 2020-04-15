4786447e63169b82754f542cfe38f56ba4e23c75
bloodstone <maks.khokholov.99@gmail.com>
2020-04-15 19:04:48 +0300

    pfpf

--- lesson4/hw2/index.js
+++ lesson4/hw2/index.js
@@ -4,14 +4,13 @@ let result = 1;
 let g = 20;
 
 do {
-    if (g % 2 != 4) {
+    if (g % 2 === 0) {
         result += g;
     }
     g++;
 
     if (g % 3 === 0) {
         result -= g;
-
     }
 
     if (g % 4 === 0) {
