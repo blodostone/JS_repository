0693e6934a64ef1503f9452b8986da1d11882647
bloodstone <maks.khokholov.99@gmail.com>
2020-04-16 00:05:49 +0300

    vc

--- lesson4/hw2/index.js
+++ lesson4/hw2/index.js
@@ -1,28 +1,27 @@
 const m = 20;
 const n = 30;
-let result = 1;
-let g = 20;
+let result = 0;
 
-do {
+for (let g = m; g <= n; g++) {
     if (g % 2 === 0 && g % 4 != 0) {
         result += g;
-    }
-    g++;
-    if (g % 3 === 0) {
+        if (g % 5 === 0) {
+            console.log(g);
+        }
+    } else if (g % 3 === 0) {
         result -= g;
-    }
-
-    if (g % 4 === 0) {
+        if (g % 5 === 0) {
+            console.log(g);
+        }
+    } else if (g % 4 === 0) {
         result *= g;
+        if (g % 5 === 0) {
+            console.log(g);
+        }
+    } else if (g % 5 === 0) {
+        console.log(g);
+    } else {
+        continue;
     }
 
-
-    if (g % 5 === 0) {
-        result = g;
-        console.log(g)
-    }
-
-} while (g <= n) {
-    result = g;
 }
-console.log('Result: ');
