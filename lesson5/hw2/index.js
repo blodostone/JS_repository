611b177b5951c87eed6863fecbcd0501a7de6a31
bloodstone <maks.khokholov.99@gmail.com>
2020-04-16 20:21:55 +0300

    fdb

---
+++ lesson5/hw2/index.js
@@ -0,0 +1,18 @@
+function isPrime(m) {
+    for (let i = 2; i < m; i += 1) {
+        if (m % i === 0) {
+            return false;
+        }
+        return true;
+    }
+}
+
+function getPrimes(n) {
+    for (let i = 2; i <= n; i += 1) {
+        if (isPrime(i)) {
+            console.log(i);
+        }
+    }
+}
+
+console.log(getPrimes(10));
