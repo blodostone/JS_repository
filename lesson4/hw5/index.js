834aaeb750ca41bcdce343333e192a7af70b04b3
bloodstone <maks.khokholov.99@gmail.com>
2020-04-16 17:30:23 +0300

    ada

---
+++ lesson4/hw5/index.js
@@ -0,0 +1,28 @@
+// открыли цикл перебераем от 0 до 1к 
+// после это присвоевоем в переменную
+// созд. переменную и это результ 
+
+//присвоить  на 1234
+
+let max = 1000;
+let result = 0;
+
+for (let min = 0; min <= max; min++) {
+    result += min;
+}
+
+let int = 0;
+int = result / 1234;
+console.log(int);
+
+let get = 0;
+get = result % 1234;
+console.log(get);
+
+switch (result) {
+    case (true):
+        int <= get;
+    case (false):
+        get <= int;
+    default:
+}
