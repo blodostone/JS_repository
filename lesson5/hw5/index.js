d1b8542bffe50927c2e345fa6c2452e13aa3c44f
bloodstone <maks.khokholov.99@gmail.com>
2020-04-20 17:26:34 +0300

    hfb

--- lesson5/hw5/index.js
+++ lesson5/hw5/index.js
@@ -1,9 +1,10 @@
 function findDivCount(a, b, n) {
-    if ([a, b] = n) {
-        return true;
-    } else {
-        return false;
+    let result = 0;
+    for (let g = a; g <= b; g++) {
+        if (g % n === 0) {
+            result += 1;
+        }
     }
+    return result;
 }
-
-console.log(findDivCount(4, 5, 9))
+console.log(findDivCount(1, 12, 2))
