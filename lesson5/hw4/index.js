6f365b69736ca52e5a0d44903c300822432bd924
bloodstone <maks.khokholov.99@gmail.com>
2020-04-20 15:39:47 +0300

    gdfb

---
+++ lesson5/hw4/index.js
@@ -0,0 +1,13 @@
+function sum(from, to) {
+
+}
+
+function compareSums(a, b, c, d) {
+    if ([a, b] > [c, d]) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(compareSums(12, 13, 6, 5))
