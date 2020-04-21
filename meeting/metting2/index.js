9004926d298144525c39724405a2d666c9b9b4bf
bloodstone <maks.khokholov.99@gmail.com>
2020-04-21 11:40:00 +0300

    fsv

--- meeting/metting2/index.js
+++ meeting/metting2/index.js
@@ -3,8 +3,6 @@
 
 function some(arr) {
     for (let g = 0; g < arr.length; g += 1) {
-        console.log('==> run');
-
         if (arr[g] % 3 !== 0) {
             return true;
         }
@@ -13,7 +11,7 @@ function some(arr) {
     return false;
 }
 
-const numbers = [32, 56, 3, 4, 55];
+const numbers = [32, 56, 31, 4, 55];
 
 const result = some(numbers);
 
