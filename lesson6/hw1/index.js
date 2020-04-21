22f9681d69a16837b43e90c4b3493ab7b099bdec
bloodstone <maks.khokholov.99@gmail.com>
2020-04-21 16:32:04 +0300

    sdfv

--- lesson6/hw1/index.js
+++ lesson6/hw1/index.js
@@ -1,11 +1,11 @@
 function createEmptyArray(len) {
     let emptyArray = [];
     for (let g = 0; g <= len.length; g++) {
-        emptyArray.push('');
+        emptyArray.push();
     }
     return emptyArray;
 }
 
-const arrayEmpty = createEmptyArray(8);
+const arrayEmpty = createEmptyArray([]);
 
 console.log(arrayEmpty);
