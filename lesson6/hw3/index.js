a41c3627ba5a3572dfc1c3578fee581ae53849de
bloodstone <maks.khokholov.99@gmail.com>
2020-04-21 13:43:09 +0300

    cvc

---
+++ lesson6/hw3/index.js
@@ -0,0 +1,17 @@
+function checkSum(arr) {
+    let sum = 0;
+    for (let g = 0; g < arr.length; g++) {
+        sum += arr[g];
+        if (g) {
+
+        }
+    }
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+}
+
+let number = (23, 13, 9, 42)
+const sum = checkSum(number);
+
+console.log(sum);
