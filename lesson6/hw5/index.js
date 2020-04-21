436cfaa5f041171a18f03ea6fc0451da7394a588
bloodstone <maks.khokholov.99@gmail.com>
2020-04-21 17:20:29 +0300

    lolu

---
+++ lesson6/hw5/index.js
@@ -0,0 +1,11 @@
+const number = [2, 6, 10, 14, 18]
+
+function reverseArray(arr) {
+    const reversed = arr.reverse();
+
+    console.log(reversed)
+}
+
+const reverseAnwser = reverseArray(number);
+
+console.log(reverseAnwser);
