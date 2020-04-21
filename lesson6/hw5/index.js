b05c513edfdcce8fe575f5f7bed25cdb8c47ae4b
bloodstone <maks.khokholov.99@gmail.com>
2020-04-21 17:21:25 +0300

    bbb

--- lesson6/hw5/index.js
+++ lesson6/hw5/index.js
@@ -1,6 +1,9 @@
 const number = [2, 6, 10, 14, 18]
 
 function reverseArray(arr) {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
     const reversed = arr.reverse();
 
     console.log(reversed)
