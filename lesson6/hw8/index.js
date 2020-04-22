f841bb8237ff3612e5c992bb33d232e7cc3619e7
bloodstone <maks.khokholov.99@gmail.com>
2020-04-22 18:37:25 +0300

    gvz

--- lesson6/hw8/index.js
+++ lesson6/hw8/index.js
@@ -14,7 +14,32 @@ function sortAsc(array) {
             }
         }
     }
+
     return array;
 };
+
 const arrSort = sortAsc(a);
+
 console.log(arrSort)
+
+
+function sortDesc(array) {
+    if (!Array.isArray(array)) {
+        return null;
+    }
+    let len = array.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len; j++) {
+            if (array[j] < array[j + 1]) {
+                let tmp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = tmp;
+            }
+        }
+    }
+
+    return array;
+};
+const arrSort1 = sortDesc(a);
+
+console.log(arrSort1)
