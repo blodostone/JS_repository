39ca49cc64cc3230697f108144f24e6365299478
bloodstone <maks.khokholov.99@gmail.com>
2020-04-22 15:52:44 +0300

    fasv

--- lesson6/hw11/index.js
+++ lesson6/hw11/index.js
@@ -1,14 +1,31 @@
+// function includes(arr, b) {
+//     let result = 0;
+//     for (let g = 0; g <= arr.length; g++) {
+//         if (arr[g] === b) {
+//             result = true;
+//         } else {
+//             result = false;
+//         }
+//     }
+//     return result
+// }
+
+// const displaysResult = includes([1, 4, 8, 3], 3);
+// console.log(displaysResult);
+
 function includes(arr, b) {
-    let result = 0;
-    for (let g = 0; g <= arr.length; g++) {
-        if (arr[g] === b) {
-            result = true;
-        } else {
-            result = false;
+
+    let choice = false;
+    arr.forEach(function(value) {
+        if (value == b) {
+            choice = true;
         }
-    }
-    return result
+
+    });
+    return choice;
 }
 
-const displaysResult = includes([1, 4, 8, 3], 3);
+const number = [1, 4, 8, 3];
+const displaysResult = includes(number, 3)
+
 console.log(displaysResult);
