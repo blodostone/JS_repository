4a81234258b64a3f5898a200cf4f2ae45cb21649
bloodstone <maks.khokholov.99@gmail.com>
2020-04-23 23:17:12 +0300

    gdf

--- lesson7/hw3/index.js
+++ lesson7/hw3/index.js
@@ -1,10 +1,15 @@
 const increaseEvenEl = numbers => {
+    if (!Array.isArray(numbers)) {
+        return null;
+    }
+
+    const evenEl = numbers.map((num) => (num % 2 + delta));
 
 
-    const result = numbers.map(num => num * num);
-
+    return evenEl;
 }
 
-const evenElements = increaseEvenEl([2, 5, 6, 8, 11, 9, 4], 20);
+const arr = [2, 5, 6, 8, 11, 9, 4];
+const delta = 20;
 
 console.log(increaseEvenEl(arr, delta));
