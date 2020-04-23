1e2fe28060ea6e641c49df57c28e12784e4709b0
bloodstone <maks.khokholov.99@gmail.com>
2020-04-23 16:29:48 +0300

    gdfn

---
+++ lesson7/hw3/index.js
@@ -0,0 +1,10 @@
+const increaseEvenEl = numbers => {
+
+
+    const result = numbers.map(num => num * num);
+
+}
+
+const evenElements = increaseEvenEl([2, 5, 6, 8, 11, 9, 4], 20);
+
+console.log(increaseEvenEl(arr, delta));
