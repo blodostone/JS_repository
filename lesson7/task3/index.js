8d7b6b05313e367d36162329b257a25dd20a7d79
bloodstone <maks.khokholov.99@gmail.com>
2020-04-23 14:21:39 +0300

    hfnlk

---
+++ lesson7/task3/index.js
@@ -0,0 +1,15 @@
+const flatArray = arr => arr
+    .flat();
+
+// const flatArray = arr
+//     .reduce((acc, elem) => {
+//         return acc.concat(elem);
+//     }, []);
+// return flatArray;
+
+
+const initArray = [1, [2, 3, 4], 5, [6]];
+
+console.log(flatArray(initArray));
+
+console.log(initArray);
