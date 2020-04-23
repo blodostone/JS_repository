a9d9cbfa58ca4b698717f6592273985a379747a6
bloodstone <maks.khokholov.99@gmail.com>
2020-04-23 15:07:13 +0300

    fsbfgh

---
+++ lesson7/hw1/index.js
@@ -0,0 +1,15 @@
+const squareArray = numbers => {
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+
+    const result = numbers.map(num => num * num);
+
+    return result;
+}
+
+const arr = [1, 2, 3, 4, 5, 6]
+
+console.log(squareArray(arr));
+
+console.log(arr);
