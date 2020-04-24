729ab926f1422c42618ec46e4fc8b7f693ef0b07
bloodstone <maks.khokholov.99@gmail.com>
2020-04-24 15:06:40 +0300

    gdf

--- lesson7/hw7/index.js
+++ lesson7/hw7/index.js
@@ -1,11 +1,27 @@
-const filterItems = (people) => arr.filter((item) =>
-    item.toLowerCase().indexOf(people.toLowerCase()) > -1
-);
+// const filterItems = (people, text) => {
+//     const dfgdfg = .filter
+
+
+const filterItems = (people, text) => {
+    const filterPeople = people
+        .filter(name => name.includes(text));
+
+
+
+
+    return filterPeople;
+
+}
+
+
+// const filterItems = (people) => arr.filter((item) =>
+//     item.toLowerCase().indexOf(people.toLowerCase()) > -1
+// );
 
 
 const arr = ['Kolya', 'Tolya', 'Marina', 'Kate'];
 const text = ('ya');
 
-console.log(filterItems(text));
+console.log(filterItems(arr, text));
 
-console.log(arr);
+// console.log(arr);
