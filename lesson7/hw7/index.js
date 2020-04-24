60776cd446c75f5905f04f992aed5f847cdd0d99
bloodstone <maks.khokholov.99@gmail.com>
2020-04-24 14:10:09 +0300

    fgbknf

--- lesson7/hw7/index.js
+++ lesson7/hw7/index.js
@@ -1,8 +1,7 @@
-const filterItems = (people) => {
-    return arr.filter((item) =>
-        item.toLowerCase().indexOf(people.toLowerCase()) > -1
-    );
-}
+const filterItems = (people) => arr.filter((item) =>
+    item.toLowerCase().indexOf(people.toLowerCase()) > -1
+);
+
 
 const arr = ['Kolya', 'Tolya', 'Marina', 'Kate'];
 const text = ('ya');
