0cef2f14d7f1e431ab77929e042e13a6549d2940
bloodstone <maks.khokholov.99@gmail.com>
2020-04-26 22:47:19 +0300

    fdv

--- lesson8/hw2/index.js
+++ lesson8/hw2/index.js
@@ -3,24 +3,25 @@ const user = {
     age: 17,
 };
 
-/* присвойте объекту свойство hobby со значением 'football' с помощью точки */
 user.hobby = 'football';
+/* присвойте объекту свойство hobby со значением 'football' с помощью точки */
+
 
 const anotherKey = 'married';
 /* название свойства хранится в переменной anotherKey. Присвойте объекту такое свойство со значением false */
 user.married = false;
 
-/* присвойте объекту свойство 'favorite music' со значением 'rock' */
 user['favorite music'] = 'rock';
+/* присвойте объекту свойство 'favorite music' со значением 'rock' */
 
-
-/* присвойте объекту свойство 'address', которое будет пустым объкетом */
 user.adress = {};
+/* присвойте объекту свойство 'address', которое будет пустым объкетом */
 
-/* присвойте объекту в 'address' свойство 'country', со значением 'Ukraine' */
 user.adress.country = "Ukraine";
+/* присвойте объекту в 'address' свойство 'country', со значением 'Ukraine' */
 
-/* присвойте объекту в 'address' свойство 'building', со значением 17 */
 user.adress.building = 17;
+/* присвойте объекту в 'address' свойство 'building', со значением 17 */
+
 /* выведите в консоль объект user */
 console.log(user);
