25875072ddeeee538418859bd7bec2fae006c96d
bloodstone <maks.khokholov.99@gmail.com>
2020-04-26 22:49:30 +0300

    gdfb

--- lesson8/hw2/index.js
+++ lesson8/hw2/index.js
@@ -14,13 +14,13 @@ user.married = false;
 user['favorite music'] = 'rock';
 /* присвойте объекту свойство 'favorite music' со значением 'rock' */
 
-user.adress = {};
+user.address = {};
 /* присвойте объекту свойство 'address', которое будет пустым объкетом */
 
-user.adress.country = "Ukraine";
+user.address.country = "Ukraine";
 /* присвойте объекту в 'address' свойство 'country', со значением 'Ukraine' */
 
-user.adress.building = 17;
+user.address.building = 17;
 /* присвойте объекту в 'address' свойство 'building', со значением 17 */
 
 /* выведите в консоль объект user */
