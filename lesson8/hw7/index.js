45c43a177ad84b6805221d15891cc5abc96c6522
bloodstone <maks.khokholov.99@gmail.com>
2020-04-28 00:08:54 +0300

    frgv

--- lesson8/hw7/index.js
+++ lesson8/hw7/index.js
@@ -1,45 +1,17 @@
 const obj1 = {
     name: 'Tom',
-    age: 17,
+    age: 17
 };
 const obj2 = {
-    name: 'Bob',
-    age: 17,
-};
-const obj3 = {
-    name: 'Bob',
-    age: 17,
-    student: false
-};
-const obj4 = {
     name: 'Tom',
-    age: 17,
+    age: 17
 };
-
 
 function compareObjects(obj1, obj2) {
-    if (obj1 === obj2) {
+    if (JSON.stringify(obj1) === JSON.stringify(obj2)) {
         return true;
-    }
-    return false;
+    } else
+        return false;
 }
-
-function compareObjects(obj2, obj3) {
-    if (obj2 === obj3) {
-        return true;
-    }
-    return false;
-}
-
-function compareObjects(obj1, obj4) {
-    if (obj1 == obj4) {
-        return true;
-    }
-    return false;
-}
-
-
-
-console.log(compareObjects(obj1, obj2));
-console.log(compareObjects(obj2, obj3));
-console.log(compareObjects(obj1, obj4));
+const result = compareObjects(obj1, obj2)
+console.log(result)
