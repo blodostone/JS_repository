6cca69e016775ca99abc0abb16e9fdffc0656196
bloodstone <maks.khokholov.99@gmail.com>
2020-04-29 01:16:56 +0300

    gdb

--- lesson8/hw6/index.js
+++ lesson8/hw6/index.js
@@ -16,12 +16,12 @@ const mergeObjectsV1 = (obj1, obj2) => {
 const result = mergeObjectsV1(obj1, obj2);
 console.log(result);
 
-const mergeObjectsV2 = (obj2, obj1) => {
+const mergeObjectsV2 = (obj1, obj2) => {
     const assignObject2 = Object.assign({}, (obj2), obj1);
     return assignObject2;
 }
 
-const resulted = mergeObjectsV2(obj2, obj1);
+const resulted = mergeObjectsV2(obj1, obj2);
 console.log(resulted);
 
 const mergeObjectsV3 = (obj1, obj2) => {
@@ -31,9 +31,9 @@ const mergeObjectsV3 = (obj1, obj2) => {
 const result3 = mergeObjectsV3(obj1, obj2);
 console.log(result3);
 
-const mergeObjectsV4 = (obj2, obj1) => {
+const mergeObjectsV4 = (obj1, obj2) => {
     const assignObject4 = {...obj2, ...obj1 };
     return assignObject4;
 }
-const result4 = mergeObjectsV4(obj2, obj1);
+const result4 = mergeObjectsV4(obj1, obj2);
 console.log(result4);
