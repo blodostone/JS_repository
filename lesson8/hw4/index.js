fbff19925eaf8b36f651cf6c64a9b9c06027b2cc
bloodstone <maks.khokholov.99@gmail.com>
2020-04-29 10:52:22 +0300

    fhn

--- lesson8/hw4/index.js
+++ lesson8/hw4/index.js
@@ -4,11 +4,12 @@ const userData = {
 
 
 const addPropertyV1 = (userData, userId) => {
-    const firstProperty = userData.id = userId;
-    return firstProperty;
+    userData.id = userId;
+    return userData;
 }
 const result = addPropertyV1(userData, 2345);
 console.log(result);
+
 
 
 
@@ -17,12 +18,22 @@ const addPropertyV2 = (userData, userId) => {
     const secondProperty = Object.assign(userData, { id: userId });
     return secondProperty;
 }
-const result1 = addPropertyV2(userData, 2345);
-console.log(result1);
+const result2 = addPropertyV2(userData, 2345);
+console.log(result2);
+
 
 const addPropertyV3 = (userData, userId) => {
-    const thirdProperty = Object.assign(userData, { id: userId });
+    const thirdProperty = Object.assign({}, userData, { id: userId });
     return thirdProperty;
 }
-const result2 = addPropertyV3(userData, 2345);
-console.log(result2);
+const result3 = addPropertyV3(userData, 2345);
+console.log(result3);
+
+
+const addPropertyV4 = (userData, userId) => {
+    const firstProperty = {...userData, id: userId };
+    return firstProperty;
+
+}
+const result4 = addPropertyV4(userData, 2345);
+console.log(result4);
