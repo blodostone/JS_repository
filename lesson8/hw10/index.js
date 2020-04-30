f3a75b860b4e246fc1cce7a36ead51cee168c1f2
bloodstone <maks.khokholov.99@gmail.com>
2020-04-30 14:19:09 +0300

    gdb

---
+++ lesson8/hw10/index.js
@@ -0,0 +1,28 @@
+const users = {
+    'John Doe': 19,
+    'Tom': 17,
+    'Bob': 18,
+}
+
+
+// const getAdults = mans => {
+//     const newAdults = mans.filter(persons => );
+//     return newAdults;
+// }
+
+// const result = getAdults(users)
+// console.log(result)
+
+
+function getAdults(users) {
+    let result = [];
+    for (let g in users) {
+        if (users[g] >= 18) {
+            result[g] = users[g];
+        }
+        return false;
+    }
+    return result;
+}
+
+console.log(getAdults(users));
