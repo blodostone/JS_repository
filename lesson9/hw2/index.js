3dd2740256734ba66ca5a2abd64a92f1cee4654a
bloodstone <maks.khokholov.99@gmail.com>
2020-05-02 03:58:59 +0300

    jghm

---
+++ lesson9/hw2/index.js
@@ -0,0 +1,28 @@
+const rooms = {
+    room1: [
+        { name: 'room1 name1' },
+        { name: 'room1 name2' },
+        { name: 'room1 name3' },
+        { name: 'room1 name4' },
+    ],
+    room2: [
+        { name: 'room2 name1' },
+    ],
+    room3: [
+        { name: 'room3 name1' },
+        { name: 'room3 name2' },
+        { name: 'room3 name3' },
+    ],
+};
+const getPeople = (obj) => {
+    if (Object.keys(obj).length === 0) {
+        return [];
+    }
+    const values = Object.values(obj);
+    values.reduce((acc, elem) => (acc = elem));
+    const arrObj = values.flat();
+    return arrObj.map((value) => value.name);
+};
+
+const result = getPeople(rooms);
+console.log(result);
