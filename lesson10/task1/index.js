3d6871a8f92f4b947ca2e3a83b876fdf9278660e
bloodstone <maks.khokholov.99@gmail.com>
2020-05-03 16:15:43 +0300

    jhg

--- lesson10/task1/index.js
+++ lesson10/task1/index.js
@@ -1,19 +1,19 @@
-getFininteNumbers = num => Number.isFinite(num);
-const result = getFininteNumbers('12');
+const getFininteNumbers = num => Number.isFinite(num);
+const result = getFininteNumbers(12, '12', -Infinity);
 console.log(result)
 
-getFininteNumbersV2 = num => isFinite(num);
+const getFininteNumbersV2 = num => isFinite(num);
 const resultV2 = getFininteNumbers('12');
 console.log(resultV2);
 
-getNaN = nan => Number.isNaN(nan);
+const getNaN = nan => Number.isNaN(nan);
 const resultNaN = getNaN(NaN);
 console.log(resultNaN);
 
-getNaNV2 = nan => isNaN(nan);
+const getNaNV2 = nan => isNaN(nan);
 const resultNaNV2 = getNaN(NaN);
 console.log(resultNaNV2);
 
-getIntegers = dontfull => Number.isInteger(dontfull);
+const getIntegers = dontfull => Number.isInteger(dontfull);
 const resultIntergers = getIntegers(1.0);
 console.log(resultIntergers);
