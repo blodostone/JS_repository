eed9e87c80a3c03823ee8bea576fc67249f9d69c
bloodstone <maks.khokholov.99@gmail.com>
2020-05-03 20:52:42 +0300

    gdb

--- lesson10/task2/index.js
+++ lesson10/task2/index.js
@@ -1,44 +1,5 @@
 const arr = [7, 3.94, 8, 67, '5.23klkl', null, NaN];
 
-
-// const getParsedIntegers = arr => {
-//     const ParsedIntegersArr = arr
-//         .map(el => Number.parselnt(el));
-//     return ParsedIntegersArr;
-// };
-
-// console.log(getParsedIntegers(arr));
-
-// const getParsedIntegersV2 = arr => {
-//     const ParsedIntegersArrV2 = arr
-//         .map(el => parselnt(el));
-//     return ParsedIntegersArrV2;
-// };
-
-// console.log(getParsedIntegersV2(arr));
-
-
-// const getParsedFloats = arr => {
-//     const ParsedFloats = arr
-//         .map(el => Number.parseFloat(el));
-//     return ParsedFloats;
-// }
-// console.log(getParsedFloats(arr));
-
-// const getParsedFloatsV2 = arr => {
-//     const ParsedFloatsV2 = arr
-//         .map(el => parseFloat(el));
-//     return ParsedFloatsV2;
-// }
-// console.log(getParsedFloatsV2(arr));
-
-
-// const compareParselnt = getParsedIntegersV2(arr) === getParsedIntegers(arr);
-
-
-// const compareParseFloat = getParsedFloats(arr) === getParsedFloatsV2(arr);
-// console.log(compareParseFloat);
-
 function getParsedFloats(nan) {
     const getNanFun = nan.map(sum => Number.parseFloat(sum));
     return getNanFun;
