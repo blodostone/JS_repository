5178054c2f9b492b2c390658eff90fd954549f49
bloodstone <maks.khokholov.99@gmail.com>
2020-05-03 21:52:10 +0300

    fdsv

--- lesson10/task3/index.js
+++ lesson10/task3/index.js
@@ -1,35 +1,32 @@
-// const multiRound = (num) => [
-//     Math.round(num * 100) / 100,
-//     Math.floor(num * 100) / 100,
-//     Math.ceil(num * 100) / 100,
-//     Math.trunc(num * 100) / 100,
-//     Number(num.toFixed(2)),
-// ];
-
-// const result = multiRound(-Math.PI);
-// console.log(result);
-
-// const result2 = multiRound(Math.PI);
-// console.log(result2);
-
-
-
-const multiRound = (num, sum) => [
+const multiRound = (num) => [
     Math.round(num * 100) / 100,
-    Math.round(sum * 100) / 100,
-
     Math.floor(num * 100) / 100,
-    Math.floor(sum * 100) / 100,
-
     Math.ceil(num * 100) / 100,
-    Math.ceil(sum * 100) / 100,
-
     Math.trunc(num * 100) / 100,
-    Math.trunc(sum * 100) / 100,
-
     Number(num.toFixed(2)),
-    Number(sum.toFixed(2)),
 ];
 
-const result3 = multiRound(-Math.PI, Math.PI);
-console.log(result3);
+console.log(multiRound(-Math.PI), multiRound(Math.PI));
+
+
+
+
+// const multiRound = (num, sum) => [
+//     Math.round(num * 100) / 100,
+//     // Math.round(sum * 100) / 100,
+
+//     // Math.floor(num * 100) / 100,
+//     // Math.floor(sum * 100) / 100,
+
+//     // Math.ceil(num * 100) / 100,
+//     // Math.ceil(sum * 100) / 100,
+
+//     // Math.trunc(num * 100) / 100,
+//     // Math.trunc(sum * 100) / 100,
+
+//     Number(num.toFixed(2)),
+//     Number(sum.toFixed(2)),
+// ];
+
+// const result3 = multiRound(-Math.PI, Math.PI);
+// console.log(result3);
