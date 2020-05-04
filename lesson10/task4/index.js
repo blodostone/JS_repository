54056158717299caf3b319d86fb7d5713e835182
bloodstone <maks.khokholov.99@gmail.com>
2020-05-04 18:01:47 +0300

    gf

--- lesson10/task4/index.js
+++ lesson10/task4/index.js
@@ -1,27 +1,30 @@
 const arr = [2, 16, -45, 54, -99];
 
-const getMaxAbsoluteNumber = arr => {
-    let max = -Infinity;
-    let now = new Array();
-
-    arr.forEach(num => {
-        if (Math.abs(num) > max) {
-            max = Math.abs(num);
-        } else if (max === now) {
-            return null;
-        }
-    })
-
-    return max;
-}
-
 // const getMaxAbsoluteNumber = arr => {
+//     let max = -Infinity;
+//     let now = new Array();
+
 //     const absoluteValues = arr
 //         .map(num => Math.abs(num));
-
-//     const max = Math.max(...absoluteValues);
+//     if (arr === now) {
+//         return null;
+//     }
 
 //     return max;
 // }
+
+const getMaxAbsoluteNumber = arr => {
+    let now = new Array();
+    if (arr === now) {
+        return null;
+    }
+
+    const absoluteValues = arr
+        .map(num => Math.abs(num));
+
+    const max = Math.max(...absoluteValues);
+
+    return max;
+}
 
 console.log(getMaxAbsoluteNumber(arr));
