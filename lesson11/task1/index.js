5f33b98ff8dba4f27405b3ee48e7610b48f7aa36
bloodstone <maks.khokholov.99@gmail.com>
2020-05-04 23:51:23 +0300

    gfd

--- lesson11/task1/index.js
+++ lesson11/task1/index.js
@@ -4,14 +4,14 @@ const splitText = (text, len) => {
 
     while (true) {
         let chunk = text.substr(startPositon, len);
+        if (!String(text)) {
+            return null;
+        }
         if (chunk.length === 0) {
             break;
         }
         strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
         startPositon += len;
-    }
-    if (text !== String) {
-        return null
     }
 
     return strArr.join('\n');
@@ -20,3 +20,4 @@ const splitText = (text, len) => {
 const result = splitText('abcdefg', 4);
 
 console.log(result);
+//'abcdefg'
