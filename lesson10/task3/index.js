a7b578e30700e43359789758916cb9e8a1135dc1
bloodstone <maks.khokholov.99@gmail.com>
2020-05-04 10:03:39 +0300

    jg

--- lesson10/task3/index.js
+++ lesson10/task3/index.js
@@ -1,31 +1,7 @@
 const multiRound = num => [
-    Math.round(num * 100) / 100,
     Math.floor(num * 100) / 100,
+    Math.round(num * 100) / 100,
     Math.ceil(num * 100) / 100,
     Math.trunc(num * 100) / 100, +num.toFixed(2),
-];
-
-const result = multiRound(-Math.PI);
-
-// console.log(result);
-
-
-// const multiRound = (num, sum) => [
-//     Math.round(num * 100) / 100,
-//     // Math.round(sum * 100) / 100,
-
-//     // Math.floor(num * 100) / 100,
-//     // Math.floor(sum * 100) / 100,
-
-//     // Math.ceil(num * 100) / 100,
-//     // Math.ceil(sum * 100) / 100,
-
-//     // Math.trunc(num * 100) / 100,
-//     // Math.trunc(sum * 100) / 100,
-
-//     Number(num.toFixed(2)),
-//     Number(sum.toFixed(2)),
-// ];
-
-// const result3 = multiRound(-Math.PI, Math.PI);
-// console.log(result3);
+]
+const result = multiRound(-Math.PI)
