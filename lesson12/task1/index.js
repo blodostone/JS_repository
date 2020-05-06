6ac0e17df4737fa1c7e7c70778a315fac90dfb3e
bloodstone <maks.khokholov.99@gmail.com>
2020-05-06 14:04:12 +0300

    hg

---
+++ lesson12/task1/index.js
@@ -0,0 +1,22 @@
+const calc = exp => {
+    const [a, operator, b] = exp.split(" ");
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = Number(a) + b;
+            break;
+        case '-':
+            result = a - b;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;
+
+    }
+    return `${exp} = ${result}`;
+};
+
+console.log(calc('1 + 2'));
