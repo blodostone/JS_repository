471bb84c2ea2171017f277a03a5f539c082e7021
bloodstone <maks.khokholov.99@gmail.com>
2020-05-06 13:41:40 +0300

    hg

--- lesson11/hw2/index.js
+++ lesson11/hw2/index.js
@@ -2,7 +2,7 @@ function countOccurrences(str, str1) {
     if (str1 === '') {
         return null
     }
-    return str.split(str1.length) - 1;
+    return (str.split(str1.length) - 1);
 }
 const result = countOccurrences('somethig in the text', '');
 // console.log(result)
