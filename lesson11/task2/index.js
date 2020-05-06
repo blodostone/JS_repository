c20ca062222fae6bcc8b23bfee3a3e8f103c0bd4
bloodstone <maks.khokholov.99@gmail.com>
2020-05-06 13:05:27 +0300

    jhn

---
+++ lesson11/task2/index.js
@@ -0,0 +1,47 @@
+const contacts = [{
+        name: 'Staso',
+        phoneNumber: '32423-324-423',
+    },
+    {
+        name: 'Peter',
+        phoneNumber: '13343-324-426',
+    },
+    {
+        name: 'Nick',
+        phoneNumber: '54356-324-428',
+    },
+];
+
+const sortContacts = (contacts, ifContacts = true) => {
+    if (!Array.isArray(contacts)) {
+        return null;
+    }
+
+    // while (true) {
+    //     const user = contacts
+    //         .sort((a, b) => {
+    //             return a.name.localeCompare(b.name);
+    //         });
+    //     if (user.length === 0) {
+    //         break;
+    //     }
+    // } 
+    const user = contacts
+        .sort((a, b) => {
+            return a.name.localeCompare(b.name);
+        });
+    if (ifContacts == false) {
+        user = contacts
+            .sort((a, b) => {
+                return b.name.localeCompare(a.name);
+            });
+    }
+    return user;
+
+
+
+}
+
+const result = sortContacts(contacts);
+
+console.log(result)
