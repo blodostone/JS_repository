fc88579467422f9b8fb967437a66fc13bd4667fb
bloodstone <maks.khokholov.99@gmail.com>
2020-05-07 02:33:16 +0300

    hb

--- lesson12/hw2/index.js
+++ lesson12/hw2/index.js
@@ -7,13 +7,14 @@ const cleanTransactionsList = (money) => {
     // const maping = money.map(sum => sum);
     const cleaningTransaction = money.filter(el => isFinite(el));
     const mapingTransaction = cleaningTransaction.map((el) => `${'$'.concat(el)}`);
+    const formatTransaction = mapingTransaction.map((el) => el.toFixed(2));
     // const mapingTransaction = cleaningTransaction.map(sum => sum.concat('$' + money));
-    return mapingTransaction;
+    return formatTransaction;
 }
 
 const result = cleanTransactionsList(['1.9', '16.4', '17', '1 dollar']);
 
-// console.log(result);
+console.log(result);
 
 
 //toFixed
