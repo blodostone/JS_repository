4125eb9cde334a76f57456b2a434172fc520e9b0
bloodstone <maks.khokholov.99@gmail.com>
2020-05-06 13:40:09 +0300

    gfb

---
+++ lesson11/hw2/index.js
@@ -0,0 +1,8 @@
+function countOccurrences(str, str1) {
+    if (str1 === '') {
+        return null
+    }
+    return str.split(str1.length) - 1;
+}
+const result = countOccurrences('somethig in the text', '');
+// console.log(result)
