77db8d5186d8b58d9a007041a4f2b059425cbc7d
bloodstone <maks.khokholov.99@gmail.com>
2020-05-06 15:22:15 +0300

    hgb

---
+++ lesson12/hw1/index.js
@@ -0,0 +1,9 @@
+const reverseString = string => {
+    if (typeof text !== 'string') {
+        return null;
+    }
+    return string.split('').reverse().join('');
+}
+const result = reverseString('AbcdeF');
+
+// console.log(result);
