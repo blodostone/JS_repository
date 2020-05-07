4218e5dfa76155147512ee305ce95756e89ebfcb
bloodstone <maks.khokholov.99@gmail.com>
2020-05-07 11:35:47 +0300

    jhb

--- lesson12/hw2/index.js
+++ lesson12/hw2/index.js
@@ -3,19 +3,8 @@
 // к каждому числу добавить знак $
 
 const cleanTransactionsList = (money) => {
-    // let arr = new Array();
-    // const maping = money.map(sum => sum);
-    const cleaningTransaction = money.filter(el => isFinite(el));
-    const mapingTransaction = cleaningTransaction.map((el) => `${'$'.concat(el)}`);
-    // const formatTransaction = mapingTransaction.map((el) => el.toFixed(2));
-    const badIfTransaction = mapingTransaction.map((el) => {
-        if (el.length <= 3) {
-            return `${el}.00`;
-        }
-        return `${el}0`;
-    })
-    return badIfTransaction;
-    // const mapingTransaction = cleaningTransaction.map(sum => sum.concat('$' + money));
+    return money.filter(el => isFinite(el))
+        .map(tr => `$${Number.parseFloat(tr).toFixed(2)}`);
 }
 
 const result = cleanTransactionsList(['1.9', '16.4', '17', '1 dollar']);
