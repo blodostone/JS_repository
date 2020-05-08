7e3f4837c98dfb42d31a658b44ccdf2286c7bed5
bloodstone <maks.khokholov.99@gmail.com>
2020-05-08 18:58:22 +0300

    hgb

--- lesson14/task1/index.js
+++ lesson14/task1/index.js
@@ -1,11 +1,19 @@
+'user strict';
+
 let message = 'Just learn it';
 
 export function sendMessage(name) {
     const sender = 'GromCode';
 
-    console.log(`${name}, ${message}! Your ${sender}`);
+    // console.log(`${name}, ${message}! Your ${sender}`);
 }
 
 export function setMessage(text) {
     message = text;
 }
+
+// sendMessage('Ann');
+
+// setMessage('Good job');
+
+// sendMessage('Ann');
