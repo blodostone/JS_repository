128f5828ae40d034f17f7c3c6efaafc85d32b809
bloodstone <maks.khokholov.99@gmail.com>
2020-05-08 19:17:44 +0300

    bv

---
+++ lesson14/task2/index.js
@@ -0,0 +1,42 @@
+let message = 'Just learn it';
+
+const createMessenger = () => {
+
+    function sendMessage(name) {
+        const sender = 'GromCode';
+
+        console.log(`${name}, ${message}! Your ${sender}`);
+    }
+
+    function setMessage(text) {
+        message = text;
+    }
+
+    function setSender(string, bold) {
+        message = string;
+        sender = bold;
+    }
+
+    return {
+        sendMessage,
+        setMessage,
+        setSender,
+    };
+};
+
+const messanger1 = createMessenger();
+
+messanger1.sendMessage('Bob');
+
+const messanger2 = createMessenger();
+
+messanger2.setMessage('Good job'.bold());
+messanger1.sendMessage('Bob');
+
+const messanger3 = createMessenger();
+
+messanger3.setSender('Just learn it');
+messanger3.setSender('GromCode'.bold());
+messanger1.sendMessage('Bob');
+
+// export default createMessenger;
