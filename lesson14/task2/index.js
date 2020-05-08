4d22a7be07929271860b91e73c40deaea6df4c26
bloodstone <maks.khokholov.99@gmail.com>
2020-05-08 21:12:44 +0300

    hgb

--- lesson14/task2/index.js
+++ lesson14/task2/index.js
@@ -24,19 +24,18 @@ const createMessenger = () => {
     };
 };
 
-const messanger1 = createMessenger();
+// const messanger1 = createMessenger();
 
-messanger1.sendMessage('Bob');
+// messanger1.sendMessage('Bob');
 
-const messanger2 = createMessenger();
+// const messanger2 = createMessenger();
 
-messanger2.setMessage('Good job'.bold());
-messanger1.sendMessage('Bob');
+// messanger2.setMessage('Good job'.bold());
+// messanger1.sendMessage('Bob');
 
-const messanger3 = createMessenger();
+// const messanger3 = createMessenger();
 
-messanger3.setSender('Just learn it');
-messanger3.setSender('GromCode'.bold());
-messanger1.sendMessage('Bob');
+// messanger3.setSender('Just learn it');
+// messanger1.sendMessage('Bob');
 
 // export default createMessenger;
