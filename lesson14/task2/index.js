dad540334a4d50b2eb970b1faf7996242328fa92
bloodstone <maks.khokholov.99@gmail.com>
2020-05-08 21:47:17 +0300

    jhn

--- lesson14/task2/index.js
+++ lesson14/task2/index.js
@@ -4,16 +4,22 @@ export default function createMessenger() {
 
     function sendMessage(name) {
         const sender = 'GromCode';
+        const name = 'Bob';
 
         console.log(`${name}, ${message}! Your ${sender}`);
     }
 
     function setMessage(text) {
         message = text.bold();
+        const sender = 'GromCode'.bold();
+
+        console.log(`${name}, ${message}! Your ${sender}`);
     }
 
     function setSender(string) {
         message = string;
+
+        console.log(`${name}, ${message}! Your ${sender}`);
     }
 
     return {
@@ -22,6 +28,30 @@ export default function createMessenger() {
         setSender,
     };
 };
+
+
+// function createMessenger() {
+
+//     function sendMessage(name) {
+//         let sender = 'GromCode';
+
+//         console.log(`${name}, ${message}! Your ${sender}`);
+//     }
+
+//     function setMessage(text) {
+//         message = text.bold();
+//     }
+
+//     function setSender(string) {
+//         message = string;
+//     }
+
+//     return {
+//         sendMessage,
+//         setMessage,
+//         setSender,
+//     };
+// };
 
 const messanger1 = createMessenger();
 
