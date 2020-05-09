3e8918b6bf0804226c9c88432ba09b745e840f36
bloodstone <maks.khokholov.99@gmail.com>
2020-05-09 18:35:24 +0300

    kjm

--- lesson14/task2/index.js
+++ lesson14/task2/index.js
@@ -1,32 +1,12 @@
 let message = 'Just learn it';
 
-export default function createMessenger() {
+let string = 'Good job!';
+let boldStr = string.bold();
 
-    function sendMessage(name) {
-        let sender = 'GromCode';
+let sender1 = 'GromCode';
+let boldSender1 = sender1.bold();
 
-        console.log(`${name}, ${message}! Your ${sender}`);
-    }
-
-    function setMessage(text) {
-        message = text.bold();
-
-    }
-
-    function setSender(string) {
-        message = string;
-
-    }
-
-    return {
-        sendMessage,
-        setMessage,
-        setSender,
-    };
-};
-
-
-// function createMessenger() {
+// export default function createMessenger() {
 
 //     function sendMessage(name) {
 //         let sender = 'GromCode';
@@ -36,10 +16,12 @@ export default function createMessenger() {
 
 //     function setMessage(text) {
 //         message = text.bold();
+
 //     }
 
 //     function setSender(string) {
 //         message = string;
+
 //     }
 
 //     return {
@@ -49,18 +31,44 @@ export default function createMessenger() {
 //     };
 // };
 
-// const messanger1 = createMessenger();
 
-// messanger1.sendMessage('Bob');
+function createMessenger() {
 
-// const messanger2 = createMessenger();
+    function sendMessage(name) {
+        let sender = 'GromCode';
 
-// messanger2.setMessage('Good job');
-// messanger1.sendMessage('Bob');
+        console.log(`${name}, ${message}! Your ${sender}`);
+    }
 
-// const messanger3 = createMessenger();
+    function setMessage(text) {
+        message = text.bold();
+    }
 
-// messanger3.setSender('Just learn it');
-// messanger1.sendMessage('Bob');
+    function setSender(string) {
+        message = string;
+        sender = boldSender1;
+    }
+
+    return {
+        sendMessage,
+        setMessage,
+        setSender,
+    };
+};
+
+const messanger1 = createMessenger();
+
+messanger1.sendMessage('Bob');
+
+const messanger2 = createMessenger();
+
+messanger2.setMessage('Good job');
+messanger1.sendMessage('Bob');
+
+const messanger3 = createMessenger();
+
+messanger3.setSender('Just learn it');
+messanger3.setSender(boldSender1);
+messanger1.sendMessage('Bob');
 
 // export default createMessenger;
