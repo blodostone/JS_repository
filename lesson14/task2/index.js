2e8ddac4e6c71a5d363998b623fa8f0d3c2a63c5
bloodstone <maks.khokholov.99@gmail.com>
2020-05-09 18:37:06 +0300

    hgb

--- lesson14/task2/index.js
+++ lesson14/task2/index.js
@@ -6,33 +6,7 @@ let boldStr = string.bold();
 let sender1 = 'GromCode';
 let boldSender1 = sender1.bold();
 
-// export default function createMessenger() {
-
-//     function sendMessage(name) {
-//         let sender = 'GromCode';
-
-//         console.log(`${name}, ${message}! Your ${sender}`);
-//     }
-
-//     function setMessage(text) {
-//         message = text.bold();
-
-//     }
-
-//     function setSender(string) {
-//         message = string;
-
-//     }
-
-//     return {
-//         sendMessage,
-//         setMessage,
-//         setSender,
-//     };
-// };
-
-
-function createMessenger() {
+export default function createMessenger() {
 
     function sendMessage(name) {
         let sender = 'GromCode';
@@ -42,11 +16,13 @@ function createMessenger() {
 
     function setMessage(text) {
         message = text.bold();
+
     }
 
     function setSender(string) {
         message = string;
         sender = boldSender1;
+
     }
 
     return {
@@ -56,19 +32,44 @@ function createMessenger() {
     };
 };
 
+
+// function createMessenger() {
+
+//     function sendMessage(name) {
+//         let sender = 'GromCode';
+
+//         console.log(`${name}, ${message}! Your ${sender}`);
+//     }
+
+//     function setMessage(text) {
+//         message = text.bold();
+//     }
+
+//     function setSender(string) {
+//         message = string;
+//         sender = boldSender1;
+//     }
+
+//     return {
+//         sendMessage,
+//         setMessage,
+//         setSender,
+//     };
+// };
+
 const messanger1 = createMessenger();
 
-messanger1.sendMessage('Bob');
+// messanger1.sendMessage('Bob');
 
 const messanger2 = createMessenger();
 
 messanger2.setMessage('Good job');
-messanger1.sendMessage('Bob');
+// messanger1.sendMessage('Bob');
 
 const messanger3 = createMessenger();
 
 messanger3.setSender('Just learn it');
 messanger3.setSender(boldSender1);
-messanger1.sendMessage('Bob');
+// messanger1.sendMessage('Bob');
 
 // export default createMessenger;
