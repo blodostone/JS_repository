575a981bb0e1a6a280c5a465ec3465686aec93d8
bloodstone <maks.khokholov.99@gmail.com>
2020-05-11 03:46:50 +0300

    ,jk

---
+++ lesson15/hw1/index.js
@@ -0,0 +1,33 @@
+function createCalculator() {
+
+    let memory = 0;
+
+    function add(plus) {
+        memory += plus;
+    }
+
+    function decrease(minus) {
+        memory -= minus;
+    }
+
+    function reset() {
+        memory = 0;
+    }
+
+    function getMemo() {
+        return memory;
+    }
+
+    return {
+        add,
+        decrease,
+        reset,
+        getMemo,
+    }
+
+}
+
+// const counter1 = createCalculator();
+// const counter2 = createCalculator();
+// const counter3 = createCalculator();
+// const counter4 = createCalculator();
