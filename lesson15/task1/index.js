76d583c58147782682bc2d1af76a3dcbe2b255bf
bloodstone <maks.khokholov.99@gmail.com>
2020-05-11 03:29:19 +0300

    bdf

--- lesson15/task1/index.js
+++ lesson15/task1/index.js
@@ -11,8 +11,8 @@ export default makeCounter;
 const counter1 = makeCounter();
 const counter2 = makeCounter();
 
-console.log(counter1());
-console.log(counter1());
-console.log(counter2());
-console.log(counter1());
-console.log(counter2());
+// console.log(counter1());
+// console.log(counter1());
+// console.log(counter2());
+// console.log(counter1());
+// console.log(counter2());
