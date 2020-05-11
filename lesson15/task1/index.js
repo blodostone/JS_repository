ed847d4e3e230059b4eb8380576576a85dbd67a8
bloodstone <maks.khokholov.99@gmail.com>
2020-05-11 03:24:17 +0300

    hgb

--- lesson15/task1/index.js
+++ lesson15/task1/index.js
@@ -9,8 +9,8 @@ function makeCounter() {
 const counter1 = makeCounter();
 const counter2 = makeCounter();
 
-console.log(counter1());
-console.log(counter1());
-console.log(counter2());
-console.log(counter1());
-console.log(counter2());
+// console.log(counter1());
+// console.log(counter1());
+// console.log(counter2());
+// console.log(counter1());
+// console.log(counter2());
