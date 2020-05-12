1dc57127db075a2b472f903eefb9e291ef363e05
bloodstone <maks.khokholov.99@gmail.com>
2020-05-12 18:26:06 +0300

    kh

---
+++ lesson17/hw1/index.js
@@ -0,0 +1,98 @@
+/* ===> 1 <=== */
+const student = {
+    name: 'Tom',
+};
+
+function sayName() {
+    console.log(this.name);
+}
+
+
+sayName.call(student);
+
+// вызовите ф-цию sayName так, чтобы в консоль вывелось имя студента 
+// ... your code here
+const otherStudent = {
+    name: 'Bruce',
+};
+
+sayName.call(otherStudent);
+
+// вызовите ф-цию sayName так, чтобы в консоль вывелось имя 'Bruce' (используйте другой объект)
+// ... your code here
+
+
+
+/* ===> 2 <=== */
+const company = {
+    companyName: 'Microsoft'
+};
+
+function greeting(firstName, lastName) {
+    console.log(`Hello, ${firstName} ${lastName}. Welcome to the ${this.companyName}`);
+}
+
+// вызовите ф-цию greeting так, чтобы в консоль вывелось 
+// 'Hello, Bob Marley. Welcome to the Microsoft'
+// используйте объект company
+// ... your code here
+
+
+greeting.call(company, 'Bob', 'Marley');
+
+
+/* ===> 3 <=== */
+const country = {
+    countryName: 'Ukraine',
+    capital: 'Kyiv',
+};
+
+function getPopulation(population) {
+    return `Population in ${this.countryName} is ${population}`;
+}
+
+// вызовите ф-цию getPopulation так, чтобы она вернула 
+// 'Population in Ukraine is 43000'
+// 43000 передавайте в виде числа
+// используйте объект country
+// результат работы ф-ции getPopulation присвойте в переменную и выведите в консоль
+// ... your code here
+
+// const func = country.getPopulation.bind({ countryName: 'Ukraine' });
+
+// const func = country.getPopulation;
+
+// func({ countryName: 'Ukraine' })
+
+// const newObject = func.call(43000);
+
+// // console.log(newObject);
+
+
+
+/* ===> 4 <=== */
+const transaction = {
+    amount: 1200,
+    operation: 'sell',
+    currency: 'USD',
+    exchange: 'NYSE',
+    printTransaction() {
+        console.log(`${this.amount} ${this.currency} - ${this.operation} on ${this.exchange}`);
+    }
+}
+
+const anotherTransaction = {
+    amount: 400,
+    operation: 'buy',
+    currency: 'USD',
+    exchange: 'NASDAQ',
+};
+
+// вызовите метод transaction.printTransaction так, чтобы в консоль вывелось
+// '400 USD - buy on NASDAQ'
+// используйте объект anotherTransaction как контекст
+// ... your code here
+
+const funcLast = transaction.printTransaction(amount);
+
+console.log(funcLast);
