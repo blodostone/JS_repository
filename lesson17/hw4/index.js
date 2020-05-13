77725085c9f581e59454ca767c514498bcd832bc
bloodstone <maks.khokholov.99@gmail.com>
2020-05-13 18:52:39 +0300

    jh

--- lesson17/hw4/index.js
+++ lesson17/hw4/index.js
@@ -1,12 +1,12 @@
-const user = {
-    firstName: 'John',
-    lastName: 'Doe',
-    getFullName(firstName, lastName) {
-        return (`${firstName} ${lastName}`);
-    },
-    setFullName(name) {
-        this.firstName = name.split()
-        this.lastName = name.split()
-    }
+ export const user = {
+     firstName: 'John',
+     lastName: 'Doe',
+     getFullName(firstName, lastName) {
+         return (`${firstName} ${lastName}`);
+     },
+     setFullName(name) {
+         this.firstName = name.split()
+         this.lastName = name.split()
+     }
 
-}
+ }
