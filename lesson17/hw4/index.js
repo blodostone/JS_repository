522fa510b8ed9de77c0ecac758f787e99223f209
bloodstone <maks.khokholov.99@gmail.com>
2020-05-13 18:50:53 +0300

    gfv

---
+++ lesson17/hw4/index.js
@@ -0,0 +1,12 @@
+const user = {
+    firstName: 'John',
+    lastName: 'Doe',
+    getFullName(firstName, lastName) {
+        return (`${firstName} ${lastName}`);
+    },
+    setFullName(name) {
+        this.firstName = name.split()
+        this.lastName = name.split()
+    }
+
+}
