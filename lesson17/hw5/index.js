e5bdd9daeb9c8e8ea30a805667ad35d4de7738bd
bloodstone <maks.khokholov.99@gmail.com>
2020-05-16 00:58:25 +0300

    gdf

--- lesson17/hw5/index.js
+++ lesson17/hw5/index.js
@@ -1,21 +1,21 @@
-const letTime = (time) => {
+const letTime = time => {
     if (time < 10) {
         return `0${time}`;
     }
     return time;
-}
+};
 export const timer = {
     minsPassed: 0,
     secondsPassed: 0,
-    getInterval: null,
+    _interval: null,
     startTimer() {
-        this.getInterval = setInterval(() => {
+        this._interval = setInterval(() => {
             this.secondsPassed += 5;
             if (this.secondsPassed === 60) {
                 this.minsPassed += 1;
                 this.secondsPassed = 0;
             }
-        }, 3000);
+        }, 5000);
     },
     resetTimer() {
         this.stopTimer();
@@ -23,8 +23,8 @@ export const timer = {
         this.secondsPassed = 0;
     },
     stopTimer() {
-        clearInterval(this.getInterval);
-        this.getInterval = null;
+        clearInterval(this._interval);
+        this._interval = null;
     },
     getTime() {
         const formatTime = letTime(this.secondsPassed);
