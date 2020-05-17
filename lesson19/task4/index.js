b0e826325db49dc5db3e5eadc4db1219c7da093a
bloodstone <maks.khokholov.99@gmail.com>
2020-05-17 23:04:38 +0300

    gdv

---
+++ lesson19/task4/index.js
@@ -0,0 +1,13 @@
+export default {
+    firstName: "John",
+    lastName: "Doe",
+    get fullName() {
+        return `${this.firstName} ${this.lastName}`;
+    },
+    set fullName(value) {
+        const [firstName, lastName] = value.split(" ");
+        this.firstName = firstName;
+        this.lastName = lastName;
+        return `${this.firstName} ${this.lastName}`;
+    },
+};
