fd7c1ba59fc611cedf5d2862cef1483ff35e6cbe
bloodstone <maks.khokholov.99@gmail.com>
2020-05-17 03:13:29 +0300

    gddv

---
+++ lesson18/task2/index.js
@@ -0,0 +1,9 @@
+const wallet = {
+    transactions: [4, 2, 6, 324, -32, 4],
+    getMax() {
+        return Math.max(...this.transactions)
+    },
+    getMin() {
+        return Math.min(...this.transactions)
+    },
+};
