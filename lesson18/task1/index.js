d916aa826e4c2d77fa111fa69c809876d9945ce2
bloodstone <maks.khokholov.99@gmail.com>
2020-05-17 03:04:39 +0300

    hfb

--- lesson18/task1/index.js
+++ lesson18/task1/index.js
@@ -3,16 +3,17 @@
 export const event = {
     guests: [
         { name: 'John', age: 18, email: 'example@sever.com' },
+        { name: 'Bob', age: 17, email: 'example@sever.com' },
     ],
     message: 'Welcome to the party!',
     getInvitations() {
         return this.guests
             .filter(({ age }) => age >= 18)
             .map(({ name, email }) => ({
-                text: `Dear ${name}!${this.message}`,
+                text: `Dear ${name}! ${this.message}`,
                 email
             }));
-    }
-}
+    },
+};
 
 // console.log(event.getInvitations());
