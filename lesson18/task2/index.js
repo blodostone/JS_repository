2df9f4aa6dc094c1fc92f054c74d2cbf7ac51af6
bloodstone <maks.khokholov.99@gmail.com>
2020-05-17 03:13:56 +0300

    jge

--- lesson18/task2/index.js
+++ lesson18/task2/index.js
@@ -1,9 +1,9 @@
-const wallet = {
-    transactions: [4, 2, 6, 324, -32, 4],
-    getMax() {
-        return Math.max(...this.transactions)
-    },
-    getMin() {
-        return Math.min(...this.transactions)
-    },
-};
+ export const wallet = {
+     transactions: [4, 2, 6, 324, -32, 4],
+     getMax() {
+         return Math.max(...this.transactions)
+     },
+     getMin() {
+         return Math.min(...this.transactions)
+     },
+ };
