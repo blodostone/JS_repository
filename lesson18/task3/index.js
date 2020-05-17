d27cd15ed4b958c46948c7cde5deef0f474950ab
bloodstone <maks.khokholov.99@gmail.com>
2020-05-17 03:28:14 +0300

    hf

--- lesson18/task3/index.js
+++ lesson18/task3/index.js
@@ -1,7 +1,7 @@
 export const sumOfSquares = sum => {
-    return [...sum].reduce((acc, elem) => {
-        return acc * elem;
-    }, 0);
+    return [...sum].map((elem) => {
+        return elem * elem;
+    });
 }
 
 const result = sumOfSquares([3, 5, 8, -16, 6]);
