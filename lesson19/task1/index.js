f48bd831ef426af634c33e04a00ca4d4f53a64dd
bloodstone <maks.khokholov.99@gmail.com>
2020-05-17 21:52:54 +0300

    fdc

--- lesson19/task1/index.js
+++ lesson19/task1/index.js
@@ -1,19 +1,19 @@
 export const vehicle = {
     name: 'Argo',
     move() {
-        console.log(`${name} is moving`);
+        console.log(`${this.name} is moving`);
     },
     stop() {
-        console.log(`${name} stopped`);
+        console.log(`${this.name} stopped`);
     }
 };
 
 export const ship = {
     startMachine() {
-        console.log(`${name} lifting anchor up`)
+        console.log(`${this.name} lifting anchor up`)
     },
     stopMachine() {
-        console.log(`${name} lifting anchor down`)
+        console.log(`${this.name} lifting anchor down`)
     },
     __proto__: vehicle
 };
