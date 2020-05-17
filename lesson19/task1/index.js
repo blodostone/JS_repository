401ee4a0357ba951b8895cb970ebad83670b065f
bloodstone <maks.khokholov.99@gmail.com>
2020-05-17 21:56:37 +0300

    hgb

--- lesson19/task1/index.js
+++ lesson19/task1/index.js
@@ -11,9 +11,11 @@ export const vehicle = {
 export const ship = {
     startMachine() {
         console.log(`${this.name} lifting anchor up`)
+        this.move();
     },
     stopMachine() {
         console.log(`${this.name} lifting anchor down`)
+        this.stop();
     },
     __proto__: vehicle
 };
