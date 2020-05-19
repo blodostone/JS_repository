aba86280ffabfc267cb27595f5777713508bc2b8
bloodstone <maks.khokholov.99@gmail.com>
2020-05-19 03:23:51 +0300

    hfb

---
+++ lesson20/task1/index.js
@@ -0,0 +1,32 @@
+export class User {
+    createUser(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    sayHi() {
+        console.log(`Hi, I am ${this.name}`);
+    }
+    requestNewPhoto() {
+        console.log(`New photo request was sent for ${this.name}`);
+    }
+    setAge(age) {
+        this.age = age;
+        if (age >= 25) {
+            console.log(`New photo request was sent for ${this.name}`);
+        } else if (age >= 0) {
+            return age;
+        } else if (age < 0) {
+            return false;
+        }
+    }
+
+    static createEmpty() {
+        return new User("", null);
+    }
+}
+
+const user1 = new User('John', 17);
+const user2 = new User('Bob', 20);
+const user3 = new User('Donald', -20);
