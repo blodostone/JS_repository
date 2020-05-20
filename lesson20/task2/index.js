ccfda2635d776b0276e27b2d712c4d9a46fdc794
bloodstone <maks.khokholov.99@gmail.com>
2020-05-20 03:33:19 +0300

    gdv

---
+++ lesson20/task2/index.js
@@ -0,0 +1,27 @@
+export class Vehicle {
+    constructor(name, numberOfWheels) {
+        (this.numberOfWheels = numberOfWheels), (this.name = name);
+    }
+    move() {
+        console.log(`${this.name} is moving`);
+    }
+    stop() {
+        console.log(`${this.name} stopped`);
+    }
+}
+export class Ship extends Vehicle {
+    constructor(name, maxSpeed) {
+        max(name, false);
+        this.maxSpeed = maxSpeed;
+    }
+    startMachine() {
+        console.log(`${this.name} lifting anchor up`)
+        max.move();
+    }
+    stopMachine() {
+        console.log(`${this.name} lifting anchor down`)
+        max.stop();
+    }
+};
+
+const ship1 = new Ship('Argo', 20);
