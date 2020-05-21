380e70c445be49b4e14cbc7bd5dddee2fa9b3043
bloodstone <maks.khokholov.99@gmail.com>
2020-05-22 01:18:40 +0300

    hfb

---
+++ lesson21/hw1/index.js
@@ -0,0 +1,12 @@
+export function getTitleElement() {
+    const titleText = document.querySelector(".title");
+    console.dir(titleText);
+    return titleText;
+}
+getTitleElement();
+export function getInputElement() {
+    const inputText = document.querySelector("input[type=text]");
+    console.dir(inputText);
+    return inputText;
+}
+getInputElement();
