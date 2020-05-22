922ce5b5ad0810d94fac2cd280f168125c3f817e
bloodstone <maks.khokholov.99@gmail.com>
2020-05-22 14:58:26 +0300

    jhn

---
+++ lesson21/hw3/index.js
@@ -0,0 +1,27 @@
+export function getTitle() {
+    const titleList = document.querySelector('.title');
+    const contentList = titleList.textContent;
+    return contentList;
+}
+getTitle()
+
+export function getDescription() {
+    const aboutList = document.querySelector('.about');
+    const innerList = aboutList.innerText;
+    return innerList;
+}
+getDescription()
+
+export function getPlans() {
+    const createPlans = document.querySelector('.plans');
+    const innerPlans = createPlans.innerText;
+    return innerPlans;
+}
+getPlans()
+
+export function getGoal() {
+    const createGoal = document.querySelector('.goal');
+    const outerGoal = createGoal.outerHTML;
+    return outerGoal;
+}
+getGoal()
