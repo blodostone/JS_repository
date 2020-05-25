e584ec5a172df444f8665734ebc10e855324ef7a
bloodstone <maks.khokholov.99@gmail.com>
2020-05-25 03:18:52 +0300

    gd

---
+++ lesson21/hw10/index.js
@@ -0,0 +1,12 @@
+export function manageClasses() {
+    const oneElem = document.querySelector(".one");
+    oneElem.classList.add("selected");
+    const twoElem = document.querySelector(".two");
+    twoElem.classList.remove("selected");
+    const threeElem = document.querySelector(".three");
+    threeElem.classList.toggle("three_done");
+    const fourElem = document.querySelector(".four");
+    if (fourElem.classList.contains("some-class") === true) {
+        fourElem.classList.add("another-class");
+    }
+}
