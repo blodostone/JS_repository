74165134cffeab472b965582f702efaea6656f8b
bloodstone <maks.khokholov.99@gmail.com>
2020-05-25 14:42:28 +0300

    hfb

--- lesson21/hw11/index.js
+++ lesson21/hw11/index.js
@@ -1,8 +1,8 @@
 export function squaredNumbers() {
     const elem = document.querySelectorAll("[data-number]");
     for (let g = 0; g < element.length; g++) {
-        let number = elem.dataset.number * elem.dataset.number;
-        elem.setAttribute("data-squared-number", number);
+        let number = elem[g].dataset.number * elem[g].dataset.number;
+        elem[g].setAttribute("data-squared-number", number);
     }
 }
 // squaredNumbers();
