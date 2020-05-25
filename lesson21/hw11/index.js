c75324432aa823f3dd3c79d53e084e49694b816f
bloodstone <maks.khokholov.99@gmail.com>
2020-05-25 14:43:14 +0300

    gfhb

--- lesson21/hw11/index.js
+++ lesson21/hw11/index.js
@@ -1,6 +1,6 @@
 export function squaredNumbers() {
     const elem = document.querySelectorAll("[data-number]");
-    for (let g = 0; g < element.length; g++) {
+    for (let g = 0; g < elem.length; g++) {
         let number = elem[g].dataset.number * elem[g].dataset.number;
         elem[g].setAttribute("data-squared-number", number);
     }
