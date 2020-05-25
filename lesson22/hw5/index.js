6bf418636ead0dbe8ba81061e3e0686284e17b87
bloodstone <maks.khokholov.99@gmail.com>
2020-05-26 02:49:52 +0300

    jgn

---
+++ lesson22/hw5/index.js
@@ -0,0 +1,8 @@
+const elem = document.querySelector('.single-use-btn');
+
+const handler = () => {
+    console.log('clicked');
+}
+
+elem.addEventListener('click', handler);
+elem.removeEventListener('click', handler);
