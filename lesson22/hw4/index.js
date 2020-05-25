0b7829f76cb8462cb153fe7fa82bbf09363c4e02
bloodstone <maks.khokholov.99@gmail.com>
2020-05-26 02:34:46 +0300

    hfb

---
+++ lesson22/hw4/index.js
@@ -0,0 +1,9 @@
+const elem = document.querySelector('.search__btn');
+
+const inputElem = document.querySelector('search__input');
+
+
+
+elem.addEventListener('click', () => {
+    console.log(inputElem.value);
+})
