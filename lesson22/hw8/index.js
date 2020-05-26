0322607b0859fa6084123d08d2e26505e28c1d94
bloodstone <maks.khokholov.99@gmail.com>
2020-05-26 17:53:43 +0300

    hjf

--- lesson22/hw8/index.js
+++ lesson22/hw8/index.js
@@ -1,10 +1,14 @@
 const elem = document.querySelector('.task-status');
 const checkboxElem = document.querySelector('[type = checkbox]');
 
-elem.addEventListener('checked', (event) => {
-    if (checkboxElem.checked === true) {
+elem.addEventListener('change', (event) => {
+    if (event.target.checked === true) {
         console.log(true);
-    } else if (checkboxElem.checked === false) {
+    } else if (event.target.checked === false) {
         console.log(false);
     }
 });
+
+elem.addEventListener('change', (event) => {
+    console.log(event.target.checked);
+})
