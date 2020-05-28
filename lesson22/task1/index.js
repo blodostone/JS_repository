67affdcf1d92159d8c357a3294c2c76bb519c8e0
bloodstone <maks.khokholov.99@gmail.com>
2020-05-28 16:46:22 +0300

    gdv

--- lesson22/task1/index.js
+++ lesson22/task1/index.js
@@ -52,8 +52,7 @@ const btnElem = document.querySelector('.clear-btn');
 btnElem.addEventListener('click', clearNullEvents);
 
 
-
-function myElement() {
+function turnOffElement() {
     elem.addEventListener('click', logGreyDiv, true);
     elem.addEventListener('click', logGreenDiv);
 
@@ -64,4 +63,19 @@ function myElement() {
     spanElem.addEventListener('click', logGreySpan);
 }
 const btnRemoveElem = document.querySelector('.remove-handlers-btn');
-btnRemoveElem.addEventListener('click', myElement);
+btnRemoveElem.addEventListener('click', turnOffElement);
+
+
+
+function turnOnElement() {
+    elem.addEventListener('click', logGreyDiv, true);
+    elem.addEventListener('click', logGreenDiv);
+
+    pElem.addEventListener('click', logGreyP, true);
+    pElem.addEventListener('click', logGreyP);
+
+    spanElem.addEventListener('click', logGreySpan, true);
+    spanElem.addEventListener('click', logGreySpan);
+}
+const btnOnElem = document.querySelector('.attach-handlers-btn');
+btnOnElem.addEventListener('click', turnOnElement);
