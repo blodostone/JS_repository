766dc7bdd59e9177e9ba5d916ecf1197df04e5de
bloodstone <maks.khokholov.99@gmail.com>
2020-05-29 17:21:30 +0300

    hfut

---
+++ lesson22/hw10/index.js
@@ -0,0 +1,26 @@
+//todo list algo
+
+//1. create array of events - and create 5 test events (создаем место для хранения данных)
+//2. write render fuction that render all tasks - function renderTasks (tasks)
+//3. addEventsListener for all
+//4. checkbox handler - update task in the array and render tasks (обновляет)
+//5. create handler - create task object and put to the array and render tasks (добавляет)
+
+
+
+
+
+const tasks = [
+    { text: 'Buy milk', done: false },
+    { text: 'Pick up Tom from airport', done: false },
+    { text: 'Visit party', done: false },
+    { text: 'Visit doctor', done: true },
+    { text: 'Buy meat', done: true },
+];
+
+
+function renderTasks(renderElems) {
+    const listRender = document.querySelector('.list');
+
+
+}
---
+++ lesson23/task1/index.js
@@ -0,0 +1,65 @@
+// // так можно получить данные формы
+// const formFields = [...new FormData(formElem)];
+// // formFields => [["email", "значение поля email"], ["password", "значение поля password"]]
+// const formData = formFields.reduce(function(acc, formField) {
+//     const prop = formField[0]; // здесь "name" инпута
+//     const value = formField[0]; // здесь "value" инпута
+//     // если использовать деструктуризацию, то можно предыдущие 2 строки записать короче
+//     // const [prop, value] = formField;
+//     return {
+//         // используем оператор расширения, чтобы в acc добвить свойства все предыдущих итераций
+//         ...acc,
+//         // используем вычислимое свойство объекта, чтобы добавить в аккумулятор новое свойство
+//         [prop]: value,
+//     };
+// }, {});
+
+
+const emailInputElem = document.querySelector('#mail');
+const passwordInputElem = document.querySelector('#password');
+const emailErrorElem = document.querySelector('.error-text_email');
+const passwordErrorElem = document.querySelector('.error-text_password');
+
+const isRequared = (value) => (value ? underfind : 'Required');
+const isEmail = (value) =>
+    value.includes('@') ? underfind : 'Should be an email';
+
+const validatorsByField = {
+    email: [isRequared, isEmail],
+    password: [isRequared]
+};
+
+const validate = (fieldName, value) => {
+    const validators = validatorsByField[fieldName];
+    return validators
+        .map((validator) => validator(value))
+        .filter((errorText) => errorText)
+        .join(', ');
+};
+
+const onEmailChange = (event) => {
+    const errorText = validate("email", event.target.value);
+    emailErrorElem.textContent = errorText;
+    console.log(event.target.value);
+};
+
+const onPasswordChange = (event) => {
+    const errorText = validate("password", event.target.value);
+    passwordErrorElem.textContent = errorText;
+    console.log(event.target.value);
+};
+
+emailInputElem.addEventListener("input", onEmailChange);
+passwordInputElem.addEventListener("input", onPasswordChange);
+
+const formElem = document.querySelector(".login-form");
+
+const onFormSubmit = (event) => {
+    event.preventDefault();
+    const formData = [...new FormData(formElem)]
+        .reduce((acc, [field, value]) => ({...acc, [field]: value }), {});
+
+    alert(JSON.stringify(formData));
+};
+
+formElem.addEventListener("submit", onFormSubmit);
