b11aabb2e9b49cbc7f877a2c8233fae22aba221b
bloodstone <maks.khokholov.99@gmail.com>
2020-06-02 22:16:38 +0300

    jgn

---
+++ lesson27/task1/index.js
@@ -0,0 +1,18 @@
+localStorage.setItem("names", JSON.stringify({ name: "Tom" }));
+localStorage.setItem("name", "Tom");
+
+export const getLocalStorageData = () => {
+    return Object.entries(localStorage).reduce((acc, [key, value]) => {
+        let newValue;
+        try {
+            newValue = JSON.parse(value);
+        } catch (e) {
+            newValue = value;
+        }
+        return {
+            ...acc,
+            [key]: newValue,
+        };
+    }, {});
+};
+// console.log(getLocalStorageData());
