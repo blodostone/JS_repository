c72e147d96521ab1f22475b492081327ef95307e
bloodstone <maks.khokholov.99@gmail.com>
2020-06-04 18:53:29 +0300

    iyk

---
+++ lesson29/task1/index.js
@@ -0,0 +1,30 @@
+const addImage = (imgSrc, callback) => {
+    const imgElem = document.createElement('img');
+    imgElem.setAttribute('alt', 'My Photo');
+    imgElem.src = imgSrc;
+    const containerElem = document.querySelector('.page');
+    containerElem.append(imgElem);
+
+    const omImageloaded = () => {
+        const { width, height } = imgElem;
+        callback(null, { width, height });
+    };
+
+    imgElem.addEventListener('load', onImageLoaded);
+
+    imgElem.addEventListener('error', () => callback('Image load failed'))
+};
+
+const imgSrc = 'htttp://server.com/image.png';
+
+const onImageLoaded = (error, date) => {
+    if (error) {
+        console.log(error);
+        return;
+    }
+    const { width, height } = date;
+    const sizeElem = document.querySelector('.image-size');
+    sizeElem.textContent = `${width} x ${height}`
+}
+
+addImage(imgSrc, onImageLoaded)
