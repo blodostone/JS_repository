16f7933fcc1e90010e94e6582c53d1e077bd689c
bloodstone <maks.khokholov.99@gmail.com>
2020-06-04 20:40:23 +0300

    hfe

---
+++ lesson29/task3/index.js
@@ -0,0 +1,9 @@
+export const printing = () => {
+    console.log(1);
+    setTimeout(function() { console.log(2) }, 1000);
+    console.log(3);
+    setTimeout(function() { console.log(4) }, 0);
+    console.log(5);
+};
+
+printing();
