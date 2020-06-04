fe23586472d8b06fb4eda9db3d6ef662e9744562
bloodstone <maks.khokholov.99@gmail.com>
2020-06-04 20:43:32 +0300

    gdwe

--- lesson29/task3/index.js
+++ lesson29/task3/index.js
@@ -1,8 +1,8 @@
 export const printing = () => {
     console.log(1);
-    setTimeout(function() { console.log(4) }, 1000);
+    setTimeout(function() { console.log(5) }, 1000);
     console.log(2);
-    setTimeout(function() { console.log(5) }, 0);
+    setTimeout(function() { console.log(4) }, 0);
     console.log(3);
 };
 
