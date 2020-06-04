8abab1ca19b2f3b9852deac061de10cc0e515504
bloodstone <maks.khokholov.99@gmail.com>
2020-06-04 20:42:11 +0300

    yyi

--- lesson29/task3/index.js
+++ lesson29/task3/index.js
@@ -1,9 +1,9 @@
 export const printing = () => {
     console.log(1);
-    setTimeout(function() { console.log(2) }, 1000);
+    setTimeout(function() { console.log(4) }, 1000);
+    console.log(2);
+    setTimeout(function() { console.log(5) }, 0);
     console.log(3);
-    setTimeout(function() { console.log(4) }, 0);
-    console.log(5);
 };
 
 printing();
