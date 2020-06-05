4f9ef5db2f6564fbc68ca56bc7d75a0222b087c4
bloodstone <maks.khokholov.99@gmail.com>
2020-06-05 20:50:42 +0300

    hfb

---
+++ lesson29/hw1/index.js
@@ -0,0 +1,23 @@
+//create function requestUserData (userId, callback)
+//create obj 
+//callback with first error ( put some text )
+//call callback
+
+
+
+const requestUserData = (userId, callback) => {
+    if (userId == 'broken') {
+        callback(null, 'Failed to load user data');
+        return;
+    }
+
+    const user = { userId: `${userId}`, email: `${userId}@example.com` };
+    // setInterval(() => callback(user), randomElem);
+    // let randomElem = parseInt(Math.random()
+
+    setTimeout(() => callback(user), Math.ceil(Math.random() * 3 * 1000));
+
+}
+
+
+// requestUserData('superUser');
