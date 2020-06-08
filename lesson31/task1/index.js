506fc7b69e41e87f372674fe3d832c13c239b0cd
bloodstone <maks.khokholov.99@gmail.com>
2020-06-09 01:09:35 +0300

    jgn

---
+++ lesson31/task1/index.js
@@ -0,0 +1,24 @@
+export const requestUserData = (userId) => {
+    const request = new Promise((resolve, reject) => {
+        if (userId === "broken") {
+            setTimeout(() => {
+                reject(new Error("User not found"));
+            }, 500);
+        } else {
+            setTimeout(() => {
+                resolve({
+                    name: "John",
+                    age: 17,
+                    userId: `${userId}`,
+                    email: `${userId}@example.com`,
+                });
+            }, 1000);
+        }
+    });
+
+    return request;
+};
+requestUserData("user777")
+    .then((data) => console.log(data))
+    .catch((error) => console.log(error))
+    .finally(() => console.log('finally'));
