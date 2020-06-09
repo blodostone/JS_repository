7e0d82fad7199ad62d571a64780cf6e25c3d1a68
bloodstone <maks.khokholov.99@gmail.com>
2020-06-09 12:29:48 +0300

    hft

---
+++ lesson31/hw3/index.js
@@ -0,0 +1,9 @@
+const delay = (time) =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(time);
+        }, time);
+    });
+
+delay(3000).then(() =>
+    console.log('Done'));
