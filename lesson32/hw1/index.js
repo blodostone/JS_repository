71e19df086c2f0b6949b9bc43267661f0ba6285e
bloodstone <maks.khokholov.99@gmail.com>
2020-06-11 02:57:33 +0300

    nhf

---
+++ lesson32/hw1/index.js
@@ -0,0 +1,11 @@
+  /*
+   * ф-ция makePromise должна вернуть промис со значением переданным в ф-цию
+   */
+
+  export const makePromise = number => Promise.resolve(number)
+      /*
+       * пример использования
+       */
+  makePromise(17).then(number => {
+      console.log(number); // 17
+  });
