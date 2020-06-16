2b759b459cf2c0c22c31957de162dc6741bfa21d
bloodstone <maks.khokholov.99@gmail.com>
2020-06-16 15:55:09 +0300

    hgf

---
+++ lesson35/task1/index.js
@@ -0,0 +1,44 @@
+window.addEventListener("error", function onUnhandledError(err) {
+    console.error("error", err.message);
+});
+
+// try {
+//   const jsonString = '{name:"Tom"';
+//   throw new ReferenceError();
+//   const userData = JSON.parse(jsonString);
+//   console.log(userData);
+// } catch (err) {
+//   if (err instanceof SyntaxError) {
+//     console.error('Headled');
+//   } else {
+//     throw err;
+//   }
+//   console.error(err.name);
+// } finally {
+//   console.log('Some actions');
+// }
+
+
+let userParsingResult;
+
+try {
+    const user = JSON.parse('{"name":"Tom"}');
+    console.log("User data: ", user);
+    userParsingResult = "success";
+} catch (e) {
+    userParsingResult = "error";
+} finally {
+    console.log(`User parsing finished with ${userParsingResult}`);
+}
+
+let productParsingResult;
+
+try {
+    const product = JSON.parse('{"name":"Tom"');
+    console.log("Product data: ", product);
+    productParsingResult = "success";
+} catch (e) {
+    productParsingResult = "error";
+} finally {
+    console.log(`Product parsing finished with ${productParsingResult}`);
+}
